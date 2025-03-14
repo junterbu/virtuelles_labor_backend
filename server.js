558f098889c991f288f2fa158a4e6549676d6ea4
junterbu <jan.unterbuchschachner@tuwien.ac.at>
2025-03-14 09:23:30 +0100

    fileUploadUpdate

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(fileUpload({
 
 // Middleware für CORS, falls Vercel Header entfernt
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*"); // 🔥 Erlaubt Anfragen von überall
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     if (req.method === "OPTIONS") {
